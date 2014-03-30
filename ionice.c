fbd12fffb07689dcefff4a0b8e766e3d61d3c686
ilovezfs <ilovezfs@icloud.com>
2014-03-30 08:27:58 -0700

    Fake support for -c.

--- ionice.c
+++ ionice.c
@@ -2,18 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/resource.h>
 
 int main(int argc, char *argv[])
 {
+	int c;
+
 	if (argc < 2)
 	{
 		fprintf(stderr, "Usage: %s <command>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
+
+	while ((c = getopt(argc, argv, "c:")) != -1);
+
+	argc -= optind;
+	argv += optind;
+
 	setiopolicy_np(IOPOL_TYPE_DISK, IOPOL_SCOPE_PROCESS, IOPOL_THROTTLE);
-	execvp (argv[1], &argv[1]);
+	execvp (argv[0], &argv[0]);
 	int exit_status = errno ? EXIT_FAILURE : EXIT_SUCCESS;
-	perror(argv[1]);
+	perror(argv[0]);
 	exit (exit_status);
 }
